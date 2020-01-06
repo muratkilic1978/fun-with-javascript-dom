4fb8dda52943ed3673cbe70e167706ba69b6add0
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-06 07:39:23 +0100

    fun-with-javascript-dom.js and style.css were updated

--- js/fun-with-javascript-dom.js
+++ js/fun-with-javascript-dom.js
@@ -1,10 +1,18 @@
 const rep = document.querySelectorAll(".copyMe");
-console.log(rep);
-
+//console.log(rep);
+let holder = {};
 for (let i = 0; i < rep.length; i++){
     console.log(rep[i].textContent+' index: - '+i);
     rep[i].addEventListener("click",function(e){
         console.log(this);
+        holder.obj = rep[i].cloneNode(true);
+        holder.obj.style.cursor = "move";
+        holder.obj.classList.add("newClone");
+        holder.obj.style.backgroundColor = "green";
+        holder.obj.style.left = rep[i].offsetLeft + 100 + "px";
+        holder.obj.style.top =  rep[i].offsetTop + 200 + "px";
+        
+        document.body.appendChild(holder.obj);
     })
     rep[i].style.left = i * 140 + "px";
 }
